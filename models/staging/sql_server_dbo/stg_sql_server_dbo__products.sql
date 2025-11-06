46090c3b0d9a82f52874df9485f3b791162cd3c7
Pablo Espejo Hijano <pabloespejo0410@gmail.com>
2025-11-06 17:41:40 +0000

    commit

---
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__products.sql
@@ -0,0 +1,22 @@
+WITH source_data AS (
+
+    SELECT
+
+        CAST(PRODUCT_ID AS VARCHAR) AS product_id,
+        
+        -- Atributos del Producto
+        CAST(NAME AS VARCHAR) AS product_name,
+        CAST(PRICE AS FLOAT) AS unit_price,
+        CAST(INVENTORY AS NUMERIC(38, 0)) AS inventory_quantity, 
+        
+        -- Metadatos de Fivetran
+        CONVERT_TIMEZONE('UTC', _FIVETRAN_SYNCED) AS loaded_at
+
+    FROM 
+        {{ source('SQL_SERVER_DBO', 'PRODUCTS') }} 
+
+    WHERE PRODUCT_ID IS NOT NULL 
+
+)
+
+SELECT * FROM source_data
